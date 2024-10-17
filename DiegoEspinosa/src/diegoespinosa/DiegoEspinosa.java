28f8f87516c4e30cd0cf7381906a7faac55d6ac9
Diego Espinosa <diegoeespinosag18@gmail.com>
2024-10-17 11:17:20 -0500

    segundo commit

--- DiegoEspinosa/src/diegoespinosa/DiegoEspinosa.java
+++ DiegoEspinosa/src/diegoespinosa/DiegoEspinosa.java
@@ -14,7 +14,7 @@ public class DiegoEspinosa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+       System.out.println("Ejemplo de JavaManejado desde GitHub Versionado a través de Gi tCreado por <Deespinosa18>");
     }
     
 }
